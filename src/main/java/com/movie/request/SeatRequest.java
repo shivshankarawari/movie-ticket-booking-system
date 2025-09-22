96af9b14e36ff184137b018271113ef789f939bc
shivshankarawari <shivshankar.awari@gmail.com>
2025-09-22 10:18:07 +0530

    Changes in config

--- src/main/java/com/movie/request/SeatRequest.java
+++ src/main/java/com/movie/request/SeatRequest.java
@@ -38,76 +38,76 @@ public class SeatRequest {
 
     private Screen screen;
 
-    public SeatRequest(Long seatId, @Positive @NotNull String seatNumber, @NotBlank String rowName, SeatStatus status, SeatType seatType, @Positive @NotNull int seatPrice, Screen screen) {
-        this.seatId = seatId;
-        this.seatNumber = seatNumber;
-        this.rowName = rowName;
-        this.status = status;
-        this.seatType = seatType;
-        this.seatPrice = seatPrice;
-        this.screen = screen;
-    }
-
-    public SeatRequest() {
-    }
-
-    public Long getSeatId() {
-        return this.seatId;
-    }
-
-    public @Positive @NotNull String getSeatNumber() {
-        return this.seatNumber;
-    }
-
-    public @NotBlank String getRowName() {
-        return this.rowName;
-    }
-
-    public SeatStatus getStatus() {
-        return this.status;
-    }
-
-    public SeatType getSeatType() {
-        return this.seatType;
-    }
-
-    public @Positive @NotNull int getSeatPrice() {
-        return this.seatPrice;
-    }
-
-    public Screen getScreen() {
-        return this.screen;
-    }
-
-    public void setSeatId(Long seatId) {
-        this.seatId = seatId;
-    }
-
-    public void setSeatNumber(@Positive @NotNull String seatNumber) {
-        this.seatNumber = seatNumber;
-    }
-
-    public void setRowName(@NotBlank String rowName) {
-        this.rowName = rowName;
-    }
-
-    public void setStatus(SeatStatus status) {
-        this.status = status;
-    }
-
-    public void setSeatType(SeatType seatType) {
-        this.seatType = seatType;
-    }
-
-    public void setSeatPrice(@Positive @NotNull int seatPrice) {
-        this.seatPrice = seatPrice;
-    }
-
-    public void setScreen(Screen screen) {
-        this.screen = screen;
-    }
-
-    public String toString() {
-        return "SeatRequest(seatId=" + this.getSeatId() + ", seatNumber=" + this.getSeatNumber() + ", rowName=" + this.getRowName() + ", status=" + this.getStatus() + ", seatType=" + this.getSeatType() + ", seatPrice=" + this.getSeatPrice() + ", screen=" + this.getScreen() + ")";
-    }
+//    public SeatRequest(Long seatId, @Positive @NotNull String seatNumber, @NotBlank String rowName, SeatStatus status, SeatType seatType, @Positive @NotNull int seatPrice, Screen screen) {
+//        this.seatId = seatId;
+//        this.seatNumber = seatNumber;
+//        this.rowName = rowName;
+//        this.status = status;
+//        this.seatType = seatType;
+//        this.seatPrice = seatPrice;
+//        this.screen = screen;
+//    }
+//
+//    public SeatRequest() {
+//    }
+//
+//    public Long getSeatId() {
+//        return this.seatId;
+//    }
+//
+//    public @Positive @NotNull String getSeatNumber() {
+//        return this.seatNumber;
+//    }
+//
+//    public @NotBlank String getRowName() {
+//        return this.rowName;
+//    }
+//
+//    public SeatStatus getStatus() {
+//        return this.status;
+//    }
+//
+//    public SeatType getSeatType() {
+//        return this.seatType;
+//    }
+//
+//    public @Positive @NotNull int getSeatPrice() {
+//        return this.seatPrice;
+//    }
+//
+//    public Screen getScreen() {
+//        return this.screen;
+//    }
+//
+//    public void setSeatId(Long seatId) {
+//        this.seatId = seatId;
+//    }
+//
+//    public void setSeatNumber(@Positive @NotNull String seatNumber) {
+//        this.seatNumber = seatNumber;
+//    }
+//
+//    public void setRowName(@NotBlank String rowName) {
+//        this.rowName = rowName;
+//    }
+//
+//    public void setStatus(SeatStatus status) {
+//        this.status = status;
+//    }
+//
+//    public void setSeatType(SeatType seatType) {
+//        this.seatType = seatType;
+//    }
+//
+//    public void setSeatPrice(@Positive @NotNull int seatPrice) {
+//        this.seatPrice = seatPrice;
+//    }
+//
+//    public void setScreen(Screen screen) {
+//        this.screen = screen;
+//    }
+//
+//    public String toString() {
+//        return "SeatRequest(seatId=" + this.getSeatId() + ", seatNumber=" + this.getSeatNumber() + ", rowName=" + this.getRowName() + ", status=" + this.getStatus() + ", seatType=" + this.getSeatType() + ", seatPrice=" + this.getSeatPrice() + ", screen=" + this.getScreen() + ")";
+//    }
 }
